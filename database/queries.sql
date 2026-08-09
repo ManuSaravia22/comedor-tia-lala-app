aa7fd7dbf2ca46cb17c92327548e0352ae814af0
Manuel <ss25027@ues.edu.sv>
2026-08-08 21:02:06 -0600

    Agregue consultas que me serviran

--- database/queries.sql
+++ database/queries.sql
@@ -5,3 +5,8 @@ JOIN clientes ON pedidos.id_cliente = clientes.id_cliente
 JOIN detalle_pedido ON pedidos.id_pedido = detalle_pedido.id_pedido
 JOIN menu_diario ON detalle_pedido.id_menu = menu_diario.id_menu
 JOIN catalogo_platillos ON menu_diario.id_platillo = catalogo_platillos.id_platillo;
+-comsultas que tambien me serviran
+SELECT 
+  SUM(CASE WHEN tipo_movimiento = 'CARGO' THEN monto ELSE -monto END) AS saldo_fiado
+FROM historial_credito
+WHERE id_cliente = 1;
