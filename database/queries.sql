03588870879ea4295b15385594917b8e0505f951
Manuel <ss25027@ues.edu.sv>
2026-08-08 20:52:09 -0600

    Guarde la consulta necesaria para trarer la info necesaria

---
+++ database/queries.sql
@@ -0,0 +1,7 @@
+-trae todo lo que necesito mostrar
+SELECT pedidos.id_pedido, clientes.nombre AS cliente, catalogo_platillos.nombre AS platillo, detalle_pedido.cantidad, pedidos.total_pagar
+FROM pedidos
+JOIN clientes ON pedidos.id_cliente = clientes.id_cliente
+JOIN detalle_pedido ON pedidos.id_pedido = detalle_pedido.id_pedido
+JOIN menu_diario ON detalle_pedido.id_menu = menu_diario.id_menu
+JOIN catalogo_platillos ON menu_diario.id_platillo = catalogo_platillos.id_platillo;
